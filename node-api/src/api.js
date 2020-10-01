ebc08ff18cfadc683846b7d51e8e87113c4fe96e
Dan Schwerner <danie.falzon6@gmail.com>
2020-10-01 19:44:18 +0200

    Cleaned boilerplate code and added dotenv to node

--- node-api/src/api.js
+++ node-api/src/api.js
@@ -1,26 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
+
+//Page slug being used to retrieve the data for cleaner URLs.
 app.get("/page/:pageName", async (req, res) => {
     const {pageName} = req.params;
     try{
+        //Utilising wpapi for easier and cleaner calls to wordpress.
         const site = await require('wpapi').discover('http://localhost:8000/');
         try{
             const page = await site.pages().slug(pageName);
+            //Checks if any data is returned by the call. If not, returns a 404.
             if(page.length){
                 return res.json(page[0]);
             }
             return json(notFound);
         }catch(e){
-            res.status(500);
-		    return res.json(e);
+            res.status(404);
+		    return res.json("Requested page does not exist.");
         }
     } catch(e){
         res.status(500);
-        return res.json(e);
+        return res.json("jo");
     }
 });
 
-const server = app.listen(7000, () => {
+const server = app.listen(process.env.PORT || 7001, () => {
     console.log("Started at http://localhost:%d\n", server.address().port);
   });
