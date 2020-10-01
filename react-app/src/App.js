da715617e802f455cf06b417c50a50ab4a0e5178
Dan Schwerner <danie.falzon6@gmail.com>
2020-10-01 20:11:40 +0200

    Cleaning files, creating initial file structure.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header.js';
+import ContactUs from './views/ContactUs.js'
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="jumbotron">
+       test
     </div>
   );
 }
---
+++ react-app/src/views/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm.js';
+import ContactInfo from '../components/ContactInfo.js';
+
+function ContactUs() {
+    return (
+        <div className="jumbotron">
+            
+        </div>
+    )
+}
+
+export default ContactUs
