9f011b6c72f287fc76d2539b2a08c31acaa7a7c5
Dan Schwerner <danie.falzon6@gmail.com>
2020-10-02 22:20:21 +0200

    Handling contact form post with validation.

--- node-api/src/api.js
+++ node-api/src/api.js
@@ -1,16 +1,25 @@
 require("dotenv").config();
+
 const express = require("express");
-var cors = require('cors');
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { body, validationResult } = require("express-validator");
 
-app.use(cors({origin: '*'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//Page slug being used to retrieve the data for cleaner URLs.
+app.use(cors({ origin: "*" }));
+
+//GET: Dynamic get using page-slug to retrieve page data from WP.
+//JSON does not return data which isn't used by the react application.
+
 app.get("/page/:pageName", async (req, res) => {
   const { pageName } = req.params;
   try {
     //Utilising wpapi for easier and cleaner calls to wordpress.
     const site = await require("wpapi").discover("http://localhost:8000/");
+    //Issue wheree the wpapi call seems to hang if the slug does not exist.
     try {
       const page = await site.pages().slug(pageName);
       //Checks if any data is returned by the call. If not, returns a 404.
@@ -18,11 +27,15 @@ app.get("/page/:pageName", async (req, res) => {
         var newJson = {
           id: page[0].id,
           title: page[0].title.rendered,
-          content: page[0].content.rendered
+          content: page[0].content.rendered,
         };
         return res.json(newJson);
       }
-      return json(notFound);
+      //If the slug doesn't match a wordpress page.
+      res.status(404);
+      return res.json({
+        error: "404 - This isn't the page you're looking for.",
+      });
     } catch (e) {
       res.status(500);
       return res.json(e);
@@ -32,6 +45,53 @@ app.get("/page/:pageName", async (req, res) => {
     return res.json(e);
   }
 });
+
+//POST: Body will contain form data posted in the contact form.
+//Content is validated and a success message is retrieved from wordpress and sent to the user.
+app.post(
+  "/post-message",
+  [
+    //Validation middleware via express-validator
+    body("fullName").notEmpty(),
+    body("email").isEmail(),
+    body("subject").notEmpty(),
+    body("message").notEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //Get customised message from wordpress
+
+    try {
+      const site = await require("wpapi").discover("http://localhost:8000/");
+      try {
+        const page = await site.pages().slug("contact-us");
+        let wpMessageResponse;
+        if (page.length) {
+          console.log(page[0]);
+          //Return message from wordpress as a JSON also returns email.
+          //Data would then be stored in a database but this wasn't required
+          //in the provided scope.
+          const responseMsg = {
+            response: `${page[0].acf.contact_message_response}`,
+            sentBy: req.body.email
+          };
+          res.json(responseMsg);
+          res.status(404);
+        }
+
+        return res.json({
+          error:
+            "404 - Slug has been changed or the contact-us page has been deleted.",
+        });
+      } catch (error) {}
+    } catch (err) {
+      return next(e);
+    }
+  }
+);
 
 //Using dotenv package to store environment variables.
 const server = app.listen(process.env.PORT || 7001, () => {
